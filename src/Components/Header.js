35bc77e12d1f5cefbd52b9364f994d0717ad84e8
Ratan Kavade -X (rakavade - ZENSAR TECHNOLOGIES LIMITED at Cisco) <rakavade@cisco.com>
2024-02-06 16:04:38 +0530

    Create sign in sign up form, update header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,12 +2,12 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div className='absolute w-full'>
-      <div className='flex justify-between'>
+    <div className='absolute w-full z-50'>
+      <div className='p-4 bg-black bg-opacity-50'>
         <img className='w-24 cursor-pointer' src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png" alt='Logo'/>
-        <div>
+        {/* <div>
           <button className='p-4 bg-red-600 text-white'>Login</button>
-        </div>
+        </div> */}
       </div>
     </div>
   )
