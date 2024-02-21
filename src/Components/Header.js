5aee7bcc86c6d49e4efdcab2d5af1f08e42fed6b
Sanjay Tapase <sanjaytapase@Sanjays-Macbook.local>
2024-02-21 16:46:58 +0530

    Create Search GPT component, toggle GPT search and home page using redux store

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,11 +4,13 @@ import { auth } from '../Utils/firebase';
 import { useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { addUser, removeUser } from '../Utils/Slice/userSlice';
+import { toggleSearchContainer } from '../Utils/Slice/gptSlice';
 
 const Header = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const user = useSelector((store) => store.user);
+  const showSearch = useSelector((store) => store.gpt.showSearchBox)
 
   const handleSignOut = () => {
     signOut(auth).then(() => {}).catch((error) => {
@@ -34,12 +36,17 @@ const Header = () => {
     return () => unsubscibe();
   }, []);
 
+  const handleSerchContainer = (e) => {
+    dispatch(toggleSearchContainer());
+  }
+
   return (
-    <div className='absolute w-full z-50 bg-gradient-to-b from-slate-700 flex justify-between'>
+    <div className='absolute opacity-100 w-full z-50 bg-gradient-to-b from-black flex justify-between'>
       <div className='p-4 '>
         <img className='w-24 cursor-pointer' src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png" alt='Logo'/>
       </div>
       { user && <div className='flex'>
+        <button onClick={handleSerchContainer} className='p-2 bg-purple-900 rounded-lg text-white mt-4 mr-4 mb-4 '>{showSearch ? "Back to Home" : "GPT Search"}</button>
         <p className='text-white mt-6 mr-3'>Welcome {user.displayName}</p>
         <button onClick={handleSignOut} className='p-2 bg-red-700 rounded-lg text-white mt-4 mr-4 mb-4 '>Sign Out</button>
       </div>}
--- src/Utils/Constants/constants.js
+++ src/Utils/Constants/constants.js
@@ -7,3 +7,5 @@ export const API_OPTION = {
 }
 
 export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w500"
+
+export const BG_URL = "https://assets.nflxext.com/ffe/siteui/vlv3/4da5d2b1-1b22-498d-90c0-4d86701dffcc/98a1cb1e-5a1d-4b98-a46f-995272b632dd/IN-en-20240129-popsignuptwoweeks-perspective_alpha_website_medium.jpg"
